cc339c1ea86148439c2b807af7e7032642aa4e36
Jonathan Elchison <JElchison@Gmail.com>
2022-09-24 13:54:19 -0400

    Add more parenthesis groupings

--- 24.py
+++ 24.py
@@ -12,21 +12,21 @@ target = 24
 def evaluate(permutation):
     (num1, num2, num3, num4), sym1, sym2, sym3 = permutation
     
-    string = "((%d %s %d) %s  %d) %s %d " % (num1, sym1, num2, sym2, num3, sym3, num4)
-    try:
-        result = eval(string)
-        if result == target:
-            print("%s = %s" % (string, target))
-    except ZeroDivisionError:
-        pass
-
-    string = " (%d %s %d) %s (%d  %s %d)" % (num1, sym1, num2, sym2, num3, sym3, num4)
-    try:
-        result = eval(string)
-        if result == target:
-            print("%s = %s" % (string, target))
-    except ZeroDivisionError:
-        pass
+    expressions = [
+        "((%d %s   %d) %s  %d)  %s %d  ",
+        " (%d %s  (%d  %s  %d)) %s %d  ",
+        " (%d %s   %d) %s (%d   %s %d) ",
+        "  %d %s ((%d  %s  %d)  %s %d) ",
+        "  %d %s  (%d  %s (%d   %s %d))"
+    ]
+    for expression in expressions:
+        string = expression % (num1, sym1, num2, sym2, num3, sym3, num4)
+        try:
+            result = eval(string)
+            if result == target:
+                print("%s = %s" % (string, target))
+        except ZeroDivisionError:
+            pass
 
 
 for permutation in itertools.product([i for i in itertools.permutations([3, 3, 7, 8])], symbols, symbols, symbols):
