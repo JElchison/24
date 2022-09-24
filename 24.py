79aead31f76494360e65b6401f78308d8c899822
Jonathan Elchison <JElchison@Gmail.com>
2022-09-24 13:34:46 -0400

    Initial commit

---
+++ 24.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import itertools
+import sys
+
+
+numbers = [int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4])]
+symbols = ['+', '-', '*', '/']
+target = 24
+
+
+def evaluate(permutation):
+    (num1, num2, num3, num4), sym1, sym2, sym3 = permutation
+    
+    string = "((%d %s %d) %s  %d) %s %d " % (num1, sym1, num2, sym2, num3, sym3, num4)
+    try:
+        result = eval(string)
+        if result == target:
+            print("%s = %s" % (string, target))
+    except ZeroDivisionError:
+        pass
+
+    string = " (%d %s %d) %s (%d  %s %d)" % (num1, sym1, num2, sym2, num3, sym3, num4)
+    try:
+        result = eval(string)
+        if result == target:
+            print("%s = %s" % (string, target))
+    except ZeroDivisionError:
+        pass
+
+
+for permutation in itertools.product([i for i in itertools.permutations([3, 3, 7, 8])], symbols, symbols, symbols):
+    evaluate(permutation)
